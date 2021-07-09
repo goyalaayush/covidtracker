379d28b360c7483ba6300032a722b515aa35a089
goyalaayush <aayushgoyal91@gmail.com>
2021-07-09 22:38:24 +0530

    Initial commit

--- src/Statewise.js
+++ src/Statewise.js
@@ -22,7 +22,7 @@ const[search,setSearch]=useState("");
            <div><h2 className="f1 pt4">India Covid-19 Dashboard</h2>
            <div className="tc justify-center" >
        <input type="text" className="search-box mt2 mb2 pa1 ba b--green bg-lightest-blue" placeholder="Enter a State or UT" value={search} onChange={(event)=>{setSearch(event.target.value)}}></input>
-       <button class="btns ml--2 mt2 mb2 pa1 pr3 pl3 ba bg-dark-red " type="reset" value="Reset" onClick={(event)=>setSearch("")}><span className="t r">X</span></button> </div></div>
+       <button class=" btns ml--2 mt2 mb2 pa1 pr3 pl3 ba  bg-light-green  " type="reset" value="Reset" onClick={(event)=>setSearch("")}><span className="t r">X</span></button> </div></div>
    
         {
             datas.filter((cur)=>{
@@ -38,7 +38,8 @@ const[search,setSearch]=useState("");
                        <scroll>
         <div className=' overflow-auto height  w5 bg-washed-green br3 pa3 ma2 grow dib bw2 shadow-5 tc'>         
      <h2 className="pt-2 ">{cur.state}</h2>
-           <div className="dib">
+     
+           <div className="dib fixed-bottom mb5">
                             <h5 className="text-primary  a ">Confirmed: {cur.confirmed}</h5>
                             <h5 className="text-secondary b">Active: {cur.active}</h5>
                             <h5 className="text-success c ">  Recovered: {cur.recovered}</h5>
@@ -50,14 +51,14 @@ const[search,setSearch]=useState("");
                                          
                 )
 
-                else 
+                else if(cur.state=="Dadra and Nagar Haveli and Daman and Diu")
                 return ( 
                     <scroll>
      <div className=' overflow-auto height w5 bg-washed-green br3 pa3 ma2 grow dib bw2 shadow-5 tc'>         
   <h2 className="pt-2">Daman & Diu</h2>
-  <div className="dib">
+  <div className="dib fixed-bottom mb5">
                          <h5 className="text-primary a ">Confirmed: {cur.confirmed}</h5>
-                         <h5 className="text-warning b">Active: {cur.active}</h5>
+                         <h5 className="text-secondary b">Active: {cur.active}</h5>
                          <h5 className="text-success  c">  Recovered: {cur.recovered}</h5>
                          <h5 className="text-danger d">Deaths: {cur.deaths}</h5>    
                          <p >(Last Updated:<br></br> {cur.lastupdatedtime})</p>
